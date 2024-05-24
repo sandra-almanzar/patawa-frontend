7de34868179d40976d39573abe5ae079e9b8c919
Sandra Almanzar <ingsandraalmanzar@gmail.com>
2024-05-23 20:39:28 -0500

    Agrego vercel.json

--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
         setAuth(data.tokenUser);
         //redireccion
         setTimeout(() => {
-          window.location.reload();
+          window.location.();
         }, 1000);
       }
     } catch (error) {
